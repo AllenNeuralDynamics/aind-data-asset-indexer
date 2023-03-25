feb202810900edb8ac240e15902ab44c48233206
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2023-03-24 19:31:43 -0700

    feat: adds job runner

--- code/aind_data_asset_indexer/job.py
+++ code/aind_data_asset_indexer/job.py
@@ -1,36 +1,37 @@
+"""Module to facilitate running a capsule job to populate a doc store"""
+
 import json
 from datetime import datetime
 from json.decoder import JSONDecodeError
-from typing import Dict, Optional
+from typing import Dict, Union
 from urllib.parse import urlparse
 
 import boto3
 import pytz
 from aind_codeocean_api.codeocean import CodeOceanClient
 from aind_data_access_api.document_store import Client as DsClient
-from aind_data_access_api.document_store import (
-    DataAssetRecord,
-    DocumentStoreCredentials,
-)
+from aind_data_access_api.document_store import DocumentStoreCredentials
+from aind_data_access_api.models import DataAssetRecord
 from botocore.exceptions import ClientError
 
 
 class JobRunner:
+    """Class to handle populating a document store"""
+
     def __init__(
         self,
-        doc_store_secrets_name,
+        doc_store_credentials: DocumentStoreCredentials,
         collection_name,
         codeocean_token,
         codeocean_domain,
         data_asset_bucket,
     ):
+        """Class Constructor"""
         self.codeocean_client = CodeOceanClient(
             domain=codeocean_domain, token=codeocean_token
         )
         self.doc_store_client = DsClient(
-            credentials=DocumentStoreCredentials(
-                aws_secrets_name=doc_store_secrets_name
-            ),
+            credentials=doc_store_credentials,
             collection_name=collection_name,
         )
         self.data_asset_bucket = data_asset_bucket
@@ -38,7 +39,21 @@ class JobRunner:
     @staticmethod
     def _map_raw_asset_to_record(
         co_response: Dict,
-    ) -> Optional[DataAssetRecord]:
+    ) -> Union[DataAssetRecord, None]:
+        """
+        Maps a raw dataasset type to a DataAssetRecord type
+        Parameters
+        ----------
+        co_response : Dict
+          The response from code ocean when requesting a data asset
+
+        Returns
+        -------
+        Union[DataAssetRecord, None]
+          The mapped response to internal model if possible. Otherwise, None.
+
+        """
+
         source_bucket = co_response.get("sourceBucket")
         bucket = source_bucket.get("bucket") if source_bucket else None
         prefix = source_bucket.get("prefix") if source_bucket else None
@@ -58,17 +73,30 @@ class JobRunner:
                 unslashed_prefix = prefix
             location = f"s3://{bucket}/{unslashed_prefix}"
             base_record = DataAssetRecord(
-                id=data_asset_id,
-                name=name,
-                created=created_datetime,
-                location=location,
+                _id=data_asset_id,
+                _name=name,
+                _created=created_datetime,
+                _location=location,
             )
 
             return base_record
 
     def _map_processed_result_to_record(
         self, co_response: Dict
-    ) -> Optional[DataAssetRecord]:
+    ) -> Union[DataAssetRecord, None]:
+        """
+        Maps a processed type to a DataAssetRecord type
+        Parameters
+        ----------
+        co_response : Dict
+          The response from code ocean when requesting a result
+
+        Returns
+        -------
+        Union[DataAssetRecord, None]
+          The mapped response to internal model if possible. Otherwise, None.
+
+        """
         name = co_response["name"]
         data_asset_id = co_response["id"]
         prefix = data_asset_id
@@ -77,22 +105,31 @@ class JobRunner:
         created_datetime = datetime.fromtimestamp(
             created_timestamp, tz=pytz.UTC
         )
-        if prefix.endswith("/"):
-            unslashed_prefix = prefix[:-1]
-        else:
-            unslashed_prefix = prefix
-        location = f"s3://{bucket}/{unslashed_prefix}"
+        location = f"s3://{bucket}/{prefix}"
         base_record = DataAssetRecord(
-            id=data_asset_id,
-            name=name,
-            created=created_datetime,
-            location=location,
+            _id=data_asset_id,
+            _name=name,
+            _created=created_datetime,
+            _location=location,
         )
         return base_record
 
     def _map_co_response_to_record(
-        self, co_response
-    ) -> Optional[DataAssetRecord]:
+        self, co_response: Dict
+    ) -> Union[DataAssetRecord, None]:
+        """
+        Maps a response to a DataAssetRecord type
+        Parameters
+        ----------
+        co_response : Dict
+          The response from code ocean when requesting a result
+
+        Returns
+        -------
+        Union[DataAssetRecord, None]
+
+        """
+
         tags = co_response["tags"]
         data_type = co_response["type"]
 
@@ -106,8 +143,26 @@ class JobRunner:
 
     @staticmethod
     def _update_record_with_s3_json_files(
-        s3_client, bucket, s3_response, base_record: DataAssetRecord
+        s3_client: boto3.session.Session.client,
+        s3_response: Dict,
+        base_record: DataAssetRecord,
     ) -> None:
+        """
+
+        Parameters
+        ----------
+        s3_client : boto3.session.Session.client
+        s3_response : Dict
+        base_record : DataAssetRecord
+
+        Returns
+        -------
+        None
+
+        """
+        url_parts = urlparse(base_record.location)
+        bucket = url_parts.netloc
+
         json_files_in_s3_folder = [
             r["Key"]
             for r in s3_response["Contents"]
@@ -125,9 +180,23 @@ class JobRunner:
             except (ClientError, JSONDecodeError):
                 pass
 
-    def _attach_json_files_to_base_record(
-        self, s3_client, base_record: DataAssetRecord
+    def _update_base_record_with_s3_info(
+        self,
+        s3_client: boto3.session.Session.client,
+        base_record: DataAssetRecord,
     ) -> None:
+        """
+        Checks the s3 location for json files to attach to the base record
+        Parameters
+        ----------
+        s3_client : boto3.session.Session.client
+        base_record : DataAssetRecord
+
+        Returns
+        -------
+        None
+
+        """
         url_parts = urlparse(base_record.location)
         bucket = url_parts.netloc
         prefix = url_parts.path[1:]
@@ -139,10 +208,12 @@ class JobRunner:
         contents_from_s3 = s3_response.get("Contents")
         if contents_from_s3 is not None:
             self._update_record_with_s3_json_files(
-                s3_client, bucket, s3_response, base_record
+                s3_client, s3_response, base_record
             )
 
     def run_job(self) -> None:
+        """Method to run the doc store populate job."""
+
         # Get data asset information from code ocean
         code_ocean_response = self.codeocean_client.search_all_data_assets()
         base_records = [
@@ -164,7 +235,7 @@ class JobRunner:
         # Attach top-level json files to base_records
         s3_client = boto3.client("s3")
         for record in base_records:
-            self._attach_json_files_to_base_record(s3_client, record)
+            self._update_base_record_with_s3_info(s3_client, record)
         s3_client.close()
         self.doc_store_client.upsert_list_of_records(base_records)
 
