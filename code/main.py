0db44824f32d328a203c77a56e9aae18793d126a
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2023-05-16 18:51:41 -0700

    feat: updates test

--- code/main.py
+++ code/main.py
@@ -7,34 +7,24 @@ import sys
 
 from aind_data_access_api.document_store import DocumentStoreCredentials
 from aind_data_asset_indexer.job import JobRunner
-from botocore.credentials import (
-    InstanceMetadataFetcher,
-    InstanceMetadataProvider,
-)
 from dotenv import load_dotenv
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
     # Load endpoints and secrets from dotenv file
     dotenv_path = Path(os.path.dirname(os.path.realpath(__file__))) / ".env"
     load_env_file = load_dotenv(dotenv_path=dotenv_path)
-    sys.stderr.write("Starting job")
-    # logging.info("Starting job: ")
+    logging.info("Starting job: ")
 
     # Load aws credentials. If not set by secrets, use instance assumed role
     if os.getenv("AWS_ACCESS_KEY_ID") is None:
-        sys.stderr.write("Checking IAM role: ")
-        provider = InstanceMetadataProvider(
-            iam_role_fetcher=InstanceMetadataFetcher(
-                timeout=10, num_attempts=2
-            )
+        logging.info("Using service account credentials")
+        os.environ["AWS_ACCESS_KEY_ID"] = os.getenv("AWS_ACCESS_KEY_ID_SVC")
+        os.environ["AWS_SECRET_ACCESS_KEY"] = (
+            os.getenv("AWS_SECRET_ACCESS_KEY_SVC")
         )
-        sys.stderr.write(f"Provider: {provider}")
-        creds = provider.load().get_frozen_credentials()
-        sys.stderr.write(f"Creds Access Key: {creds.access_key}")
-        os.environ["AWS_ACCESS_KEY_ID"] = creds.access_key
-        os.environ["AWS_SECRET_ACCESS_KEY"] = creds.secret_key
+        os.environ["AWS_DEFAULT_REGION_SVC"] = os.getenv("AWS_DEFAULT_REGION_SVC")
 
     doc_store_credentials = DocumentStoreCredentials(
         aws_secrets_name=os.getenv("DOC_STORE_SECRETS_NAME")
@@ -51,4 +41,4 @@ if __name__ == "__main__":
 
     # Run the job
     job.run_job()
-    sys.stderr.write("Finished job.")
+    logging.info("Finished job.")
