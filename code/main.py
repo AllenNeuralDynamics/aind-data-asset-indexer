3c17b3f9972e1c198931b7bf169d7bfbf40ed6f6
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2023-09-28 17:27:04 -0700

    feat: removes .env file

--- code/main.py
+++ code/main.py
@@ -1,35 +1,37 @@
 """Main entry point that will be run when Reproducible run is requested."""
 
 import logging
-import os
-from pathlib import Path
 
 from aind_codeocean_api.codeocean import CodeOceanClient
 from aind_codeocean_api.credentials import CodeOceanCredentials
 from aind_data_access_api.document_db import MetadataDbClient
 from aind_data_asset_indexer.job import JobRunner
-from dotenv import load_dotenv
+
+# Code Ocean Pipelines pulls from git. We can add non-secret params here.
+# TODO: Think of a better way to store capsule parameters for pipeline runs.
+DOC_DB_HOST = "api.allenneuraldynamics.org"
+DOC_DB_DATABASE = "metadata"
+DOC_DB_COLLECTION = "data_assets"
+CODEOCEAN_SECRETS_NAME = "/aind/prod/codeocean/credentials/readonly"
+DATA_ASSET_BUCKET = "codeocean-s3datasetsbucket-1u41qdg42ur9"
 
 if __name__ == "__main__":
-    # Load endpoints and secrets from dotenv file
-    dotenv_path = Path(os.path.dirname(os.path.realpath(__file__))) / ".env"
-    load_env_file = load_dotenv(dotenv_path=dotenv_path)
     co_client = CodeOceanClient.from_credentials(
         CodeOceanCredentials(
-            aws_secrets_name=os.getenv("CODEOCEAN_SECRETS_NAME")
+            aws_secrets_name=CODEOCEAN_SECRETS_NAME
         )
     )
     doc_db_client = MetadataDbClient(
-        host=os.getenv("DOC_DB_HOST"),
-        database=os.getenv("DOC_DB_DATABASE"),
-        collection=os.getenv("DOC_DB_COLLECTION"),
+        host=DOC_DB_HOST,
+        database=DOC_DB_DATABASE,
+        collection=DOC_DB_COLLECTION,
     )
 
     # Create a JobRunner class
     job = JobRunner(
         doc_db_client=doc_db_client,
         codeocean_client=co_client,
-        data_asset_bucket=os.getenv("DATA_ASSET_BUCKET"),
+        data_asset_bucket=DATA_ASSET_BUCKET,
     )
 
     # Run the job
