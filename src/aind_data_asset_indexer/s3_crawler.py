bf817416bd4454b69d92d4106221a2e6a4c655dc
Mekhla Kapoor <54870020+mekhlakapoor@users.noreply.github.com>
2023-11-20 18:21:42 -0800

    linters

--- src/aind_data_asset_indexer/s3_crawler.py
+++ src/aind_data_asset_indexer/s3_crawler.py
@@ -1,10 +1,11 @@
 """Module to crawl through s3"""
 
-import subprocess
-import pandas as pd
 import os
-from aind_data_access_api.rds_tables import RDSCredentials
+import subprocess
+
+import pandas as pd
 from aind_data_access_api.rds_tables import Client as RDSClient
+from aind_data_access_api.rds_tables import RDSCredentials
 
 
 class AnalyticsJobRunner:
@@ -15,8 +16,10 @@ class AnalyticsJobRunner:
         REDSHIFT_SECRETS_NAME = os.getenv("REDSHIFT_SECRETS_NAME")
         BUCKETS = os.getenv("BUCKETS")
         self.table_name = os.getenv("TABLE_NAME")
-        rds_credentials = RDSCredentials(aws_secrets_name=REDSHIFT_SECRETS_NAME)
-        bucket_str = BUCKETS.split(',')
+        rds_credentials = RDSCredentials(
+            aws_secrets_name=REDSHIFT_SECRETS_NAME
+        )
+        bucket_str = BUCKETS.split(",")
 
         self.redshift_client = RDSClient(credentials=rds_credentials)
         self.buckets_list = [item.strip() for item in bucket_str]
@@ -32,34 +35,30 @@ class AnalyticsJobRunner:
         output_filepath: str
            Filepath for output file (should include filetype? ex: folders.txt)
         """
-        download_command_str_bucket_to_local_filepath = (
-            [
-                "aws",
-                "s3",
-                "ls",
-                f"{bucket_name}",
-                str(">>"),
-                f"{output_filepath}"
-            ]
-        )
+        download_command_str_bucket_to_local_filepath = [
+            "aws",
+            "s3",
+            "ls",
+            f"{bucket_name}",
+            str(">>"),
+            f"{output_filepath}",
+        ]
         subprocess.run(download_command_str_bucket_to_local_filepath)
 
     @staticmethod
     def _download_metadata_files(bucket_name, output_directory) -> None:
         """Downloads metadata.nd.jsons to output directory"""
-        sync_metadata_command_str_bucket_to_local = (
-            [
-                "aws",
-                "s3",
-                "sync",
-                f"s3://{bucket_name}",
-                f"{output_directory}",
-                "--exclude",
-                str("*"),
-                "--include",
-                str("*.nd.json")
-            ]
-        )
+        sync_metadata_command_str_bucket_to_local = [
+            "aws",
+            "s3",
+            "sync",
+            f"s3://{bucket_name}",
+            f"{output_directory}",
+            "--exclude",
+            str("*"),
+            "--include",
+            str("*.nd.json"),
+        ]
         subprocess.run(sync_metadata_command_str_bucket_to_local)
 
     @staticmethod
@@ -67,48 +66,74 @@ class AnalyticsJobRunner:
         """Create a table of list of files"""
         with open(filepath, "r") as file:
             folders_list = file.readlines()
-        df = pd.DataFrame(folders_list, columns=['s3_prefix'])
+        df = pd.DataFrame(folders_list, columns=["s3_prefix"])
         # Strip whitespace and remove 'PRE' from the prefix column
-        df['s3_prefix'] = df['s3_prefix'].str.strip().str.replace('PRE ', '').str.replace('/', '')
+        df["s3_prefix"] = (
+            df["s3_prefix"]
+            .str.strip()
+            .str.replace("PRE ", "")
+            .str.replace("/", "")
+        )
         return df
 
     @staticmethod
-    def _create_dataframe_from_metadata_files(output_directory) -> pd.DataFrame:
+    def _create_dataframe_from_metadata_files(
+        output_directory,
+    ) -> pd.DataFrame:
         """Create a table of folders with metadata file"""
-        subfolders = [f.name for f in os.scandir(output_directory) if f.is_dir()]
-        return pd.DataFrame({'s3_prefix': subfolders})
+        subfolders = [
+            f.name for f in os.scandir(output_directory) if f.is_dir()
+        ]
+        return pd.DataFrame({"s3_prefix": subfolders})
 
     @staticmethod
-    def _join_dataframes(df1: pd.DataFrame, df2: pd.DataFrame, bucket_name: str) -> pd.DataFrame:
-        """Creates a table tracking whether data record folder has metadata file"""
-        merged_df = pd.merge(df1, df2, on='s3_prefix', how='left', indicator=True)
-        merged_df['metadata_bool'] = merged_df['_merge'] == 'both'
-        merged_df = merged_df.drop('_merge', axis=1)
-        merged_df['bucket_name'] = bucket_name
+    def _join_dataframes(
+        df1: pd.DataFrame, df2: pd.DataFrame, bucket_name: str
+    ) -> pd.DataFrame:
+        """Creates Dataframe to track whether metadata file exists"""
+        merged_df = pd.merge(
+            df1, df2, on="s3_prefix", how="left", indicator=True
+        )
+        merged_df["metadata_bool"] = merged_df["_merge"] == "both"
+        merged_df = merged_df.drop("_merge", axis=1)
+        merged_df["bucket_name"] = bucket_name
         return merged_df
 
     def _crawl_s3_buckets(
-            self, folders_filepath: str, metadata_directory: str
+        self, folders_filepath: str, metadata_directory: str
     ) -> pd.DataFrame:
         """
         Crawls through a s3 buckets to make analytics dataframe
         """
-        analytics_df = pd.DataFrame(columns=['s3_prefix', 'metadata_bool', 'bucket_name'])
+        analytics_df = pd.DataFrame(
+            columns=["s3_prefix", "metadata_bool", "bucket_name"]
+        )
         for bucket in self.buckets_list:
-            self._get_list_of_folders(bucket_name=bucket, output_filepath=folders_filepath)
-            self._download_metadata_files(bucket_name=bucket, output_directory=metadata_directory)
-            folders_df = self._create_dataframe_from_list_of_folders(folders_filepath)
-            metadata_df = self._create_dataframe_from_metadata_files(metadata_directory)
-            merged_df = self._join_dataframes(df1=folders_df, df2=metadata_df, bucket_name=bucket)
-            analytics_df = pd.concat([analytics_df, merged_df], ignore_index=True)
+            self._get_list_of_folders(
+                bucket_name=bucket, output_filepath=folders_filepath
+            )
+            self._download_metadata_files(
+                bucket_name=bucket, output_directory=metadata_directory
+            )
+            folders_df = self._create_dataframe_from_list_of_folders(
+                folders_filepath
+            )
+            metadata_df = self._create_dataframe_from_metadata_files(
+                metadata_directory
+            )
+            merged_df = self._join_dataframes(
+                df1=folders_df, df2=metadata_df, bucket_name=bucket
+            )
+            analytics_df = pd.concat(
+                [analytics_df, merged_df], ignore_index=True
+            )
         return analytics_df
 
     def run_job(self, folders_filepath, metadata_directory):
-        """Crawls through s3 buckets and writes metadata analysis table in redshift"""
-        analytics_df = self._crawl_s3_buckets(folders_filepath, metadata_directory)
-        self.redshift_client.overwrite_table_with_df(df=analytics_df, table_name=self.table_name)
-
-
-
-
-
+        """Writes metadata analysis table in redshift"""
+        analytics_df = self._crawl_s3_buckets(
+            folders_filepath, metadata_directory
+        )
+        self.redshift_client.overwrite_table_with_df(
+            df=analytics_df, table_name=self.table_name
+        )
--- tests/test_s3_crawler.py
+++ tests/test_s3_crawler.py
@@ -1,11 +1,13 @@
 """Tests methods AnalyticsJobRunner class"""
 
-import unittest
-from unittest.mock import patch, MagicMock, mock_open, Mock
-from aind_data_access_api.rds_tables import RDSCredentials
-from aind_data_asset_indexer.s3_crawler import AnalyticsJobRunner
-import pandas as pd
 import os
+import unittest
+from unittest.mock import MagicMock, Mock, mock_open, patch
+
+import pandas as pd
+from aind_data_access_api.rds_tables import RDSCredentials
+
+from aind_data_asset_indexer.s3_crawler import AnalyticsJobRunner
 
 
 class MockDirEntry(MagicMock):
@@ -23,28 +25,35 @@ class TestAnalyticsJobRunner(unittest.TestCase):
         password="some_rds_password",
         host="localhost_rds",
         port=123456,
-        database="some_rds_db"
+        database="some_rds_db",
     )
 
-    sample_folders_txt = "PRE modality_567890_2000-01-01_04-00-00/\n" \
-                         "PRE modality_642478_2023-01-17_13-56-29/\n" \
-                         "PRE modality_642478_2023-01-17_14-38-38/\n"
+    sample_folders_txt = (
+        "PRE modality_567890_2000-01-01_04-00-00/\n"
+        "PRE modality_642478_2023-01-17_13-56-29/\n"
+        "PRE modality_642478_2023-01-17_14-38-38/\n"
+    )
 
-    @patch.dict('os.environ', {'REDSHIFT': 'some_redshift_value',
-                               'REDSHIFT_SECRETS_NAME': 'some_secrets_value',
-                               'BUCKETS': '["bucket1", "bucket2"]',
-                               'TABLE_NAME': 'some_table_value'})
-    @patch('aind_data_asset_indexer.s3_crawler.RDSCredentials')
+    @patch.dict(
+        "os.environ",
+        {
+            "REDSHIFT": "some_redshift_value",
+            "REDSHIFT_SECRETS_NAME": "some_secrets_value",
+            "BUCKETS": '["bucket1", "bucket2"]',
+            "TABLE_NAME": "some_table_value",
+        },
+    )
+    @patch("aind_data_asset_indexer.s3_crawler.RDSCredentials")
     def setUp(self, mock_rds_credentials):
         self.mock_credentials = Mock(spec=RDSCredentials)
         mock_rds_credentials.return_value = self.sample_rds_credentials
         self.runner = AnalyticsJobRunner()
 
-    @patch('subprocess.run')
+    @patch("subprocess.run")
     def test_get_list_of_folders(self, mock_subprocess_run):
-        """Tests that """
-        bucket_name = 'test_bucket'
-        output_filepath = 'test_output.txt'
+        """Tests that"""
+        bucket_name = "test_bucket"
+        output_filepath = "test_output.txt"
 
         self.runner._get_list_of_folders(bucket_name, output_filepath)
         expected_command = [
@@ -53,146 +62,198 @@ class TestAnalyticsJobRunner(unittest.TestCase):
             "ls",
             "test_bucket",
             ">>",
-            "test_output.txt"
+            "test_output.txt",
         ]
         mock_subprocess_run.assert_called_once_with(expected_command)
 
-    @patch('subprocess.run')
+    @patch("subprocess.run")
     def test_download_metadata_files(self, mock_subprocess_run):
         """"""
-        bucket_name = 'test_bucket'
-        output_directory = 'test/directory/'
+        bucket_name = "test_bucket"
+        output_directory = "test/directory/"
 
         self.runner._download_metadata_files(bucket_name, output_directory)
-        expected_command = (
-            [
-                "aws",
-                "s3",
-                "sync",
-                "s3://test_bucket",
-                "test/directory/",
-                "--exclude",
-                "*",
-                "--include",
-                "*.nd.json"
-            ]
-        )
+        expected_command = [
+            "aws",
+            "s3",
+            "sync",
+            "s3://test_bucket",
+            "test/directory/",
+            "--exclude",
+            "*",
+            "--include",
+            "*.nd.json",
+        ]
         mock_subprocess_run.assert_called_once_with(expected_command)
 
-    @patch('builtins.open', new_callable=mock_open, read_data=sample_folders_txt)
+    @patch(
+        "builtins.open", new_callable=mock_open, read_data=sample_folders_txt
+    )
     def test_create_dataframe_from_list_of_folders(self, mock_file_open):
         """"""
-        filepath = 'test_folders.txt'
+        filepath = "test_folders.txt"
         df = self.runner._create_dataframe_from_list_of_folders(filepath)
-        expected_df = pd.DataFrame({'s3_prefix': [
-            'modality_567890_2000-01-01_04-00-00',
-            'modality_642478_2023-01-17_13-56-29',
-            'modality_642478_2023-01-17_14-38-38'
-        ]})
+        expected_df = pd.DataFrame(
+            {
+                "s3_prefix": [
+                    "modality_567890_2000-01-01_04-00-00",
+                    "modality_642478_2023-01-17_13-56-29",
+                    "modality_642478_2023-01-17_14-38-38",
+                ]
+            }
+        )
         pd.testing.assert_frame_equal(df, expected_df)
-        mock_file_open.assert_called_once_with(filepath, 'r')
+        mock_file_open.assert_called_once_with(filepath, "r")
 
-    @patch('os.scandir')
+    @patch("os.scandir")
     def test_create_dataframe_from_metadata_files(self, mock_scandir):
         """"""
-        output_directory = 'test_output_directory'
+        output_directory = "test_output_directory"
 
         # Mock the behavior of os.scandir
         mock_scandir.return_value = [
-            MockDirEntry(name='modality_567890_2000-01-01_04-00-00', is_dir=True),
-            MockDirEntry(name='modality_642478_2023-01-17_13-56-29', is_dir=True),
-            MockDirEntry(name='folders.txt', is_dir=False),
+            MockDirEntry(
+                name="modality_567890_2000-01-01_04-00-00", is_dir=True
+            ),
+            MockDirEntry(
+                name="modality_642478_2023-01-17_13-56-29", is_dir=True
+            ),
+            MockDirEntry(name="folders.txt", is_dir=False),
         ]
 
-        df = self.runner._create_dataframe_from_metadata_files(output_directory)
-        expected_df = pd.DataFrame({'s3_prefix': [
-            'modality_567890_2000-01-01_04-00-00',
-            'modality_642478_2023-01-17_13-56-29']
-        })
+        df = self.runner._create_dataframe_from_metadata_files(
+            output_directory
+        )
+        expected_df = pd.DataFrame(
+            {
+                "s3_prefix": [
+                    "modality_567890_2000-01-01_04-00-00",
+                    "modality_642478_2023-01-17_13-56-29",
+                ]
+            }
+        )
         pd.testing.assert_frame_equal(df, expected_df)
         mock_scandir.assert_called_once_with(output_directory)
 
     def test_join_dataframes(self):
         """"""
         expected_data = {
-            's3_prefix': ['modality_567890_2000-01-01_04-00-00', 'modality_642478_2023-01-17_13-56-29', 'modality_642478_2023-01-17_14-38-38'],
-            'metadata_bool': [True, False, True],
-            'bucket_name': ['modality_bucket', 'modality_bucket', 'modality_bucket']
+            "s3_prefix": [
+                "modality_567890_2000-01-01_04-00-00",
+                "modality_642478_2023-01-17_13-56-29",
+                "modality_642478_2023-01-17_14-38-38",
+            ],
+            "metadata_bool": [True, False, True],
+            "bucket_name": [
+                "modality_bucket",
+                "modality_bucket",
+                "modality_bucket",
+            ],
         }
         expected_df = pd.DataFrame(expected_data)
 
-        df1_data = {'s3_prefix': ['modality_567890_2000-01-01_04-00-00', 'modality_642478_2023-01-17_13-56-29', 'modality_642478_2023-01-17_14-38-38']}
-        df2_data = {'s3_prefix': ['modality_567890_2000-01-01_04-00-00', 'modality_642478_2023-01-17_14-38-38']}
-        bucket_name = 'modality_bucket'
+        df1_data = {
+            "s3_prefix": [
+                "modality_567890_2000-01-01_04-00-00",
+                "modality_642478_2023-01-17_13-56-29",
+                "modality_642478_2023-01-17_14-38-38",
+            ]
+        }
+        df2_data = {
+            "s3_prefix": [
+                "modality_567890_2000-01-01_04-00-00",
+                "modality_642478_2023-01-17_14-38-38",
+            ]
+        }
+        bucket_name = "modality_bucket"
 
         df1 = pd.DataFrame(df1_data)
         df2 = pd.DataFrame(df2_data)
         result_df = self.runner._join_dataframes(df1, df2, bucket_name)
         pd.testing.assert_frame_equal(result_df, expected_df)
 
-    @patch.object(AnalyticsJobRunner, '_get_list_of_folders')
-    @patch.object(AnalyticsJobRunner, '_download_metadata_files')
-    @patch.object(AnalyticsJobRunner, '_create_dataframe_from_list_of_folders')
-    @patch.object(AnalyticsJobRunner, '_create_dataframe_from_metadata_files')
-    @patch.object(AnalyticsJobRunner, '_join_dataframes')
+    @patch.object(AnalyticsJobRunner, "_get_list_of_folders")
+    @patch.object(AnalyticsJobRunner, "_download_metadata_files")
+    @patch.object(AnalyticsJobRunner, "_create_dataframe_from_list_of_folders")
+    @patch.object(AnalyticsJobRunner, "_create_dataframe_from_metadata_files")
+    @patch.object(AnalyticsJobRunner, "_join_dataframes")
     def test_crawl_s3_buckets(
-            self, mock_join_dataframes, mock_create_metadata_dataframe, mock_create_folders_dataframe,
-            mock_download_metadata, mock_get_list_of_folders
+        self,
+        mock_join_dataframes,
+        mock_create_metadata_dataframe,
+        mock_create_folders_dataframe,
+        mock_download_metadata,
+        mock_get_list_of_folders,
     ):
         """"""
-        folders_filepath = 'test_folders.txt'
-        metadata_directory = 'test_metadata_dir'
+        folders_filepath = "test_folders.txt"
+        metadata_directory = "test_metadata_dir"
 
-        # Mock the behavior of dependent methods
-        # mock_get_list_of_folders.side_effect = [
-        #     pd.DataFrame({'s3_prefix': [f'{bucket}_folder1', f'{bucket}_folder2']}) for bucket in ['modality1', 'modality2']
-        # ]
         mock_get_list_of_folders.side_effect = [None, None]
 
         mock_create_metadata_dataframe.side_effect = [
-            pd.DataFrame({'s3_prefix': [f'{directory}_folder1', f'{directory}_folder2']}) for directory in
-            ['dir1', 'dir2']
+            pd.DataFrame(
+                {"s3_prefix": [f"{directory}_folder1", f"{directory}_folder2"]}
+            )
+            for directory in ["dir1", "dir2"]
         ]
 
         mock_create_folders_dataframe.side_effect = [
-            pd.DataFrame({'s3_prefix': [f'{filepath}_folder1', f'{filepath}_folder2']}) for filepath in
-            ['file1', 'file2']
+            pd.DataFrame(
+                {"s3_prefix": [f"{filepath}_folder1", f"{filepath}_folder2"]}
+            )
+            for filepath in ["file1", "file2"]
         ]
 
         mock_download_metadata.side_effect = [None, None]
 
         mock_join_dataframes.side_effect = [
-            pd.DataFrame({'s3_prefix': [f'modality1_folder1', f'modality1_folder2'],
-                          'metadata_bool': [True, False],
-                          'bucket_name': ['modality1', 'modality1']
-            }),
-            pd.DataFrame({'s3_prefix': [f'modality2_folder1', f'modality2_folder2'],
-                          'metadata_bool': [True, False],
-                          'bucket_name': ['modality2', 'modality2']
-            })
+            pd.DataFrame(
+                {
+                    "s3_prefix": ["modality1_folder1", "modality1_folder2"],
+                    "metadata_bool": [True, False],
+                    "bucket_name": ["modality1", "modality1"],
+                }
+            ),
+            pd.DataFrame(
+                {
+                    "s3_prefix": ["modality2_folder1", "modality2_folder2"],
+                    "metadata_bool": [True, False],
+                    "bucket_name": ["modality2", "modality2"],
+                }
+            ),
         ]
-        result_df = self.runner._crawl_s3_buckets(folders_filepath, metadata_directory)
+        result_df = self.runner._crawl_s3_buckets(
+            folders_filepath, metadata_directory
+        )
 
         expected_data = {
-            's3_prefix': ['modality1_folder1', 'modality1_folder2', 'modality2_folder1', 'modality2_folder2'],
-            'metadata_bool': [True, False, True, False],
-            'bucket_name': ['modality1', 'modality1', 'modality2', 'modality2'],
+            "s3_prefix": [
+                "modality1_folder1",
+                "modality1_folder2",
+                "modality2_folder1",
+                "modality2_folder2",
+            ],
+            "metadata_bool": [True, False, True, False],
+            "bucket_name": [
+                "modality1",
+                "modality1",
+                "modality2",
+                "modality2",
+            ],
         }
         expected_df = pd.DataFrame(expected_data)
 
         # Explicitly convert column to dtype bool
-        assert result_df['metadata_bool'].apply(lambda x: isinstance(x, bool)).all()
-        expected_df['metadata_bool'] = expected_df['metadata_bool'].astype(bool)
-        result_df['metadata_bool'] = result_df['metadata_bool'].astype(bool)
+        assert (
+            result_df["metadata_bool"]
+            .apply(lambda x: isinstance(x, bool))
+            .all()
+        )
+        expected_df["metadata_bool"] = expected_df["metadata_bool"].astype(
+            bool
+        )
+        result_df["metadata_bool"] = result_df["metadata_bool"].astype(bool)
 
         # Assert that the result DataFrame is equal to the expected DataFrame
         pd.testing.assert_frame_equal(result_df, expected_df)
-
-
-
-
-
-
-
-
-
