5aa924f34e875eaa6e80cc8717abe50daade95ee
Helen Lin <helen.lin@alleninstitute.org>
2024-05-28 14:14:16 -0700

    test: add unit test for param store with docdb_secret_name

--- tests/test_models.py
+++ tests/test_models.py
@@ -99,6 +99,85 @@ class TestAindIndexBucketJobSettings(unittest.TestCase):
             "some_docdb_collection_name", job_settings.doc_db_collection_name
         )
 
+    @patch("boto3.client")
+    def test_from_from_param_store(self, mock_boto3_client):
+        """Tests class constructor from param store."""
+        mock_boto3_client.return_value.get_parameter.return_value = {
+            "Parameter": {
+                "Name": "a_param",
+                "Type": "String",
+                "Value": (
+                    '{"doc_db_secret_name": "some_docdb_secret_name",'
+                    '"doc_db_db_name":"some_docdb_dbname",'
+                    '"doc_db_collection_name":"some_docdb_collection_name",'
+                    '"s3_bucket":"some_bucket"}'
+                ),
+                "Version": 1,
+                "LastModifiedDate": datetime(
+                    2024, 5, 4, 15, 18, 29, 8000, tzinfo=timezone.utc
+                ),
+                "ARN": "arn:aws:ssm:us-west-2:000000000000:parameter/a_param",
+                "DataType": "text",
+            },
+            "ResponseMetadata": {
+                "RequestId": "RequestId",
+                "HTTPStatusCode": 200,
+                "HTTPHeaders": {
+                    "server": "Server",
+                    "date": "Tue, 14 May 2024 18:14:39 GMT",
+                    "content-type": "application/x-amz-json-1.1",
+                    "content-length": "415",
+                    "connection": "keep-alive",
+                    "x-amzn-requestid": "x-amzn-requestid",
+                },
+                "RetryAttempts": 0,
+            },
+        }
+        mock_boto3_client.return_value.get_secret_value.return_value = {
+            "ARN": (
+                "arn:aws:secretsmanager:us-west-2:000000000000:secret:"
+                "some_docdb_secret_name-1a2b3c"
+            ),
+            "Name": "some_docdb_secret_name",
+            "VersionId": "VersionId",
+            "SecretString": (
+                '{"admin_secrets": "some_admin_secrets_name",'
+                '"engine": "mongo",'
+                '"host": "some_docdb_host",'
+                '"password": "some_docdb_password",'
+                '"port": 12345,'
+                '"username": "some_docdb_username"}'
+            ),
+            "VersionStages": ["AWSCURRENT", "AWSPENDING"],
+            "CreatedDate": datetime(
+                2024, 5, 20, 18, 11, 45, 174000, tzinfo=timezone.utc
+            ),
+            "ResponseMetadata": {
+                "RequestId": "RequestId",
+                "HTTPStatusCode": 200,
+                "HTTPHeaders": {
+                    "x-amzn-requestid": "x-amzn-requestid",
+                    "content-type": "application/x-amz-json-1.1",
+                    "content-length": "600",
+                    "date": "Tue, 28 May 2024 19:53:17 GMT",
+                },
+                "RetryAttempts": 0,
+            },
+        }
+        job_settings = AindIndexBucketJobSettings.from_param_store(
+            param_store_name="a_param"
+        )
+        expected_job_settings = AindIndexBucketJobSettings(
+            s3_bucket="some_bucket",
+            doc_db_host="some_docdb_host",
+            doc_db_port=12345,
+            doc_db_password="some_docdb_password",
+            doc_db_user_name="some_docdb_username",
+            doc_db_db_name="some_docdb_dbname",
+            doc_db_collection_name="some_docdb_collection_name",
+        )
+        self.assertEqual(expected_job_settings, job_settings)
+
 
 class TestPopulateAindBucketsJobSettings(unittest.TestCase):
     """Test PopulateAindBucketsJobSettings class"""
