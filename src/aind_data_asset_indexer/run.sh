5bc9799dc8883d5331f5b5b8d028ba0d90f73e6f
jtyoung84 <104453205+jtyoung84@users.noreply.github.com>
2024-06-13 15:57:49 -0700

    feat: updates run.sh script

--- src/aind_data_asset_indexer/run.sh
+++ src/aind_data_asset_indexer/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 python src/aind_data_asset_indexer/index_aind_buckets.py --param-store-name $PARAM_STORE_NAME
+echo "Finished aind buckets. Starting code ocean bucket."
+python src/aind_data_asset_indexer/codeocean_bucket_indexer.py --param-store-name $PARAM_STORE_NAME_CO_JOB
+echo "Finished code ocean bucket."
